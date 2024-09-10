5516b526e4d683403eaf13d4a6c3cdfc97710b14
ZionOgunsola <ziono12@nycstudents.net>
2024-09-10 16:59:17 -0400

    Lab one

---
+++ Ogunsola_Zion_Lab_1.cpp
@@ -0,0 +1,36 @@
+//Zion Ogunsola
+//Lab 1
+//9/10/2024
+
+
+#include <iostream> 
+
+using namespace std;
+
+int main()// function
+{
+        cout <<"############################################################"<< endl;//outputs the statement
+        cout <<"#                                                          #"<< endl;
+        cout <<"# Ways to access the Task Manager on your Windows computer:#"<< endl;//outputs the statement
+        cout <<"#                                                          #"<< endl;
+        cout <<"#      Press the key combination Ctrl + Shift + Escape     #"<< endl;//outputs the statement
+        cout <<"#                                                          #"<< endl;
+        cout <<"#      Type “Task Manager” in the Windows Start menu search#"<< endl;//outputs the statement
+        cout <<"#                                                          #"<< endl; 
+        cout <<"############################################################"<< endl;//outputs the statement
+
+
+
+return 0; //ends the function
+}
+/*
+############################################################
+#                                                          #
+# Ways to access the Task Manager on your Windows computer:#
+#                                                          #
+#      Press the key combination Ctrl + Shift + Escape     #
+#                                                          #
+#      Type “Task Manager” in the Windows Start menu search#
+#                                                          #
+############################################################
+*/
